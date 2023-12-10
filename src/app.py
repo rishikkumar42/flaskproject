e2cd6f37a5b7c3706ed553c5eb93984ef821e97f
Rishik Kumar <rishikkumar@Rishiks-MacBook-Pro.local>
2023-12-10 13:44:01 -0500

    test 3

--- src/app.py
+++ src/app.py
@@ -44,5 +44,6 @@ def display_movies():
     return movie_list
 
 if __name__ == "__main__":
-
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku-provided port or default to 5000
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
